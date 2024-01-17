0622e526df2bf068adf5c92ce943d826a0b88014
inagaki <h.inagaki@hiyoku.co.jp>
2024-01-17 17:06:32 +0900

    sanctum OK（Postmanの動作のみ。vueからリクエスト送ると何故か419エラー発生）

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from 'axios'
 
 import App from './App.vue'
 import router from './router'
+
+axios.defaults.baseURL = import.meta.env.VITE_APP_API_ENDPOINT
+axios.defaults.withCredentials = true;
 
 const app = createApp(App)
 
--- laravel/app/Http/Kernel.php
+++ laravel/app/Http/Kernel.php
@@ -39,7 +39,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            // SPA認証時はコメントアウト解除
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
